c01a187a83c5f89e58186ebb4a0dd7af8eabfccf
lax3331 <la2life1234@gmail.com>
2024-02-02 14:44:36 +0400

    Initial Commit

---
+++ SparTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SparTest
+//
+//  Created by Nikita Kuznetsov on 02.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SparTest/SparTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparTestApp.swift
+//  SparTest
+//
+//  Created by Nikita Kuznetsov on 02.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
